0cc5e0e20afb3f1ed413e7efaf7338edf1b2e613
Solojay24 <ojoka04@gmail.com>
2022-12-08 11:23:07 -0800

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+# include <stdio.h> 
+/**
+ * main- Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	return(0)
+puts("/"programming is like building a multilingual puzzle");
+return(0);
+}
